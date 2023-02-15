3f41654055c8ca39542931dabf3cacae15ee68e5
Zilvinas Milvydas <30028170@students.southwales.ac.uk>
2023-02-15 15:16:11 +0000

    No logic yet just ideas

---
+++ Mend-It/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Display and select a branch
+2. Browse and order the main items
+3. Browse and order the accessories
+4. Cancel/Confirm the Order
+5. Pay for the order
+6. Track the order in real time (for both over the counter and online sales)
+*/
+
+/*
+If no existing files are found create template files
+branches.txt - holds all of the branches
+items.txt - all items that are sold
+orders.txt - all comfirmed and payed orders
+tracking.txt - all tracking information of orders
+*/
+
+/*Patterns to use
+ -	Factory Desing = Structure of the branches
+ -	Decorator Desing = Implementation of additional accessories
+ -	Observer Pattern = Real-time Tracking
+*/
+
+class Branch
+{
+public:
+
+};
+
+
+int main()
+{
+
+
+
+}
