f86faa1a8d20d88620fdf43504ea583c365497a2
Zilvinas Milvydas <91484577+30028170@users.noreply.github.com>
2023-02-23 20:14:46 +0000

    Update on the decorator pattern
    Trying to figure out the best way to set up the decorator pattern for the accesories.

--- Mend-It/Source.cpp
+++ Mend-It/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <time.h>
 
 using namespace std;
 
@@ -26,60 +28,50 @@ tracking.txt - all tracking information of orders
  -	Observer Pattern = Real-time Tracking
 */
 
+//Factory Pattern - Line 30 to 108
 class Branch
 {
 public:
 	virtual string getName() const = 0;
-	virtual vector<string> getStoreItems() const = 0;
-	virtual vector<string> getStoreAccesories() const = 0;
+	virtual map<string, double> getStoreItems() const = 0;
+	virtual map<string, double> getStoreAccesories() const = 0;
+	virtual ~Branch() {};
 };
 
 class Treforest : public Branch
 {
+private:
+	map<string, double> branchItems = { {"hammer", 100}, {"screwdriver", 50}, {"wrench", 30}, {"nail", 1} };
+	map<string, double> branchAccesories = { {"gloves", 5}, {"safety glasses", 10}, {"mask", 15} };
 public:
-	string getName() const override
-	{
-		return "Treforest";
-	};
-	vector<string> getStoreItems() const override 
-	{
-		return { "hammer", "screwdriver", "wrench", "nail" };
-	}
-	vector<string> getStoreAccesories() const override {
-		return { "gloves", "safety glasses", "mask" };
-	}
+	string getName() const override { return "Treforest"; }
+	map<string, double> getStoreItems() const override { return branchItems; }
+	map<string, double> getStoreAccesories() const override { return branchAccesories; }
+	~Treforest() {};
 };
 
 class Pontypridd : public Branch
 {
+private:
+	map<string, double> branchItems = { {"drill", 100}, {"saw", 50}, {"pliers", 30}, {"bolt", 1} };
+	map<string, double> branchAccesories = { {"tape measure", 5}, {"level", 10}, {"utility knife", 15} };
 public:
-	string getName() const override
-	{
-		return "Pontypridd";
-	};
-	vector<string> getStoreItems() const override 
-	{
-		return { "drill", "saw", "pliers", "bolt" };
-	}
-	vector<string> getStoreAccesories() const override {
-		return { "tape measure", "level", "utility knife" };
-	}
+	string getName() const override { return "Pontypridd"; };
+	map<string, double> getStoreItems() const override { return branchItems; }
+	map<string, double> getStoreAccesories() const override { return branchAccesories; }
+	~Pontypridd() {};
 };
 
 class Cardiff : public Branch
 {
+private:
+	map<string, double> branchItems = { {"paint", 100}, {"brush", 50}, {"roller", 30}, {"putty knife", 1} };
+	map<string, double> branchAccesories = { {"sandpaper", 5}, {"drop cloth", 10}, {"painter's tape", 15} };
 public:
-	string getName() const override
-	{
-		return "Cardiff";
-	}
-	vector<string> getStoreItems() const override
-	{
-		return { "paint", "brush", "roller", "putty knife" };
-	}
-	vector<string> getStoreAccesories() const override {
-		return { "sandpaper", "drop cloth", "painter's tape" };
-	}
+	string getName() const override{ return "Cardiff"; }
+	map<string, double> getStoreItems() const override{ return branchItems; }
+	map<string, double> getStoreAccesories() const override { return branchAccesories; }
+	~Cardiff() {};
 };
 
 class BranchFactory
@@ -116,7 +108,7 @@ public:
 class Item : public Product
 {
 public:
-	Item(string title, double price) : title_(title), price_(price) {}
+	Item(string title, double price) : title_(title), price_(price) {};
 	string description() override { return title_;  }
 	double price() override { return price_; }
 private:
@@ -127,34 +119,80 @@ private:
 class ProductDecorator : public Product
 {
 public:
-	ProductDecorator(Product* product) : product_(product) {}
+	ProductDecorator(Product* product) : product_(product) {};
 	string description() override { return product_->description(); }
 	double price() override { return product_->price(); }
 protected:
 	Product* product_;
 };
 
-/*
------------------------------------------------------------------------
-Create the pontypridd branch and print out the items that it holds
------------------------------------------------------------------------
-	Branch* Pontypridd = BranchFactory::createBranch("Pontypridd");
-	cout << Pontypridd->getName() << endl;
-	cout << "Pontypridd's Store Items: " << endl;
-	for (int i = 0; i < Pontypridd->getStoreItems().size(); i++)
-	{
-		cout << Pontypridd->getStoreItems().at(i) << ", ";
-	}
+//Add a check to only one can be selected as an option
+class NextDayDelivery : public ProductDecorator
+{
+public:
+	NextDayDelivery(Product* product) : ProductDecorator(product) {};
+	string description() override { return product_->description() + ", Next Day Delivery"; }
+	double price() override { return product_->price() + 15.50; }
+};
 
-	cout << endl;
-	cout << "Pontypridd's Store Accesories: " << endl;
-	for (int i = 0; i < Pontypridd->getStoreAccesories().size(); i++)
-	{
-		cout << Pontypridd->getStoreAccesories().at(i) << ", ";
+class ExpressDelivery : public ProductDecorator
+{
+public:
+	ExpressDelivery(Product* product) : ProductDecorator(product) {};
+	string description() override { return product_->description() + ", Express Delivery"; }
+	double price() override { return product_->price() + 5.50; }
+};
+
+class Accessory : public ProductDecorator
+{
+public:
+	Accessory(Product* product, string accesory, double price) : ProductDecorator(product) {};
+	string description() override { return product_->description() + ", Accessory: " << accesory; }
+	double price() override { return product_->price(); }
+};
+
+/*
+
+Creates the pontypridd branch and iterates through the items map
+
+	Branch* Pontypridd = BranchFactory::createBranch("Pontypridd");
+	map<string, double>::iterator it;
+
+	// Iterate using iterator in for loop
+	for (const auto& element : Pontypridd->getStoreItems()) {
+		string itemName = element.first;
+		double itemPrice = element.second;
+
+
+		cout << "Order" << endl;
+		Product* item = new Item(itemName, itemPrice);
+		cout << item->description() << " : " << item->price() << endl;
+
+		Product* nextDay = new NextDayDelivery(item);
+		cout << nextDay->description() << " : " << nextDay->price() << endl;
+
+		delete nextDay;
 	}
 */
 
 int main()
 {
+	Branch* Pontypridd = BranchFactory::createBranch("Pontypridd");
+	map<string, double>::iterator it;
 
+	// Iterate using iterator in for loop
+	for (const auto& element : Pontypridd->getStoreItems()) {
+		string itemName = element.first;
+		double itemPrice = element.second;
+
+
+		cout << "Order" << endl;
+		Product* item = new Item(itemName, itemPrice);
+		cout << item->description() << " : " << item->price() << endl;
+
+		Product* nextDay = new NextDayDelivery(item);
+		cout << nextDay->description() << " : " << nextDay->price() << endl;
+
+		delete nextDay;
+	}
 }
